4140f7db83fa44d7c577fcfd8c08cf11763cd726
RohithReddyByreddy <byreddyrohith@gmail.com>
2024-04-22 16:44:29 -0700

    adding code

---
+++ src/utils/requests.ts
@@ -0,0 +1,61 @@
+import {
+  PaginatedRequestParams,
+  PaginatedResponse,
+  RequestByEmployeeParams,
+  SetTransactionApprovalParams,
+  Transaction,
+  Employee,
+} from "./types"
+import mockData from "../mock-data.json"
+
+const TRANSACTIONS_PER_PAGE = 5
+
+const data: { employees: Employee[]; transactions: Transaction[] } = {
+  employees: mockData.employees,
+  transactions: mockData.transactions,
+}
+
+export const getEmployees = (): Employee[] => data.employees
+
+export const getTransactionsPaginated = ({
+  page,
+}: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
+  if (page === null) {
+    throw new Error("Page cannot be null")
+  }
+
+  const start = 0
+  const end = (page+1)*TRANSACTIONS_PER_PAGE
+
+  if (start > data.transactions.length) {
+    throw new Error(`Invalid page ${page}`)
+  }
+
+  const nextPage = end < data.transactions.length ? page + 1 : null
+
+  return {
+    nextPage,
+    data: data.transactions.slice(start, end),
+  }
+}
+
+export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+  if (!employeeId) {
+    throw new Error("Employee id cannot be empty")
+  }
+
+  return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
+}
+
+export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams): void => {
+  console.log(value)
+  const transaction = data.transactions.findIndex(
+    (currentTransaction) => currentTransaction.id === transactionId
+  )
+
+  if (transaction === -1) {
+    throw new Error("Invalid transaction to approve")
+  }
+
+  data.transactions[transaction].approved = value
+}
